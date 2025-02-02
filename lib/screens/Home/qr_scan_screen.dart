51c87cda8e3a2fa58eb0224eaed4026935f0e0dd
Umberto Della Monica <umberto.dellamonica@hotmail.it>
2025-02-02 14:26:59 +0100

    update qr_code library
    - deprecated use of library
    - use of new library version

--- lib/screens/Home/qr_scan_screen.dart
+++ lib/screens/Home/qr_scan_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner_plus/qr_code_scanner_plus.dart';
 import 'package:wifi_iot/wifi_iot.dart';
 
 class QRScanScreen extends StatefulWidget {
